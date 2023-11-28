23542962e61da012b4cd2c414f252f4effe64584
dalkilicyasin <Yd123456+*>
2023-11-28 22:13:25 +0300

    Initial Commit

---
+++ callwords/callwordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  callwordsApp.swift
+//  callwords
+//
+//  Created by yasin on 28.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct callwordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
